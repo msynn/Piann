49318608f33b1b89ba15b282138169a67409f9c0
msynn <94026200+msynn@users.noreply.github.com>
2021-12-26 22:08:44 +0930

    a
    tes

---
+++ days15.py
@@ -0,0 +1,12 @@
+#Contoh Penggunaan While Looping
+
+count = 0
+while (count < 9):
+    print("The Count is : ", count)
+    count = count + 1
+print("Good Bye!")
+
+#Contoh list dengan perulangan for
+makanan = ["Nasi Goreng", "Ayam Geprek", "Bakso", "Mie ayam"]
+for i in makanan:
+    print("Saya Suka Makan", i)
