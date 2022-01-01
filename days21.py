de7cd57a13f141a14e6e70b5237db8380db9175e
msynn <94026200+msynn@users.noreply.github.com>
2022-01-01 13:56:53 +0800

    fungsi

---
+++ days21.py
@@ -0,0 +1,18 @@
+#Cara mendefinisikan fungsi
+def fungsi ():
+    print("Ini adalah Fungsi")
+
+#Memanggis fungsi
+fungsi()
+
+#function
+def suaraorang():
+    print("Bang Beli bakso...")
+    bayar = 15000
+    print(bayar)
+
+def total():
+    suaraorang()
+    print("jadiin 2 Bungkus bang!") 
+    
+total()
