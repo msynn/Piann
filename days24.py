4aea99a80ca00a96f356f270624870ff1d6b9742
msynn <94026200+msynn@users.noreply.github.com>
2022-01-04 19:55:23 +0800

    type data set

---
+++ days24.py
@@ -0,0 +1,13 @@
+#Type Data Set
+#Tidak Memiliki indeks
+#Hanya bisa menampilkan nilai yang berbeda
+angka = {100,100,200,250,200,100,150,400,250}
+nama = {'Piann','Sudah','Makan','Siang','sore','malam','Sudah'}
+
+print(angka)
+print(nama)
+
+a1 = {1,2,4,3,9,6,7}
+a2 = {6,8,10,20,42,13}
+print(a1|a2)
+print(a1&a2)
