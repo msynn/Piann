b8a6978863cd031d6c8adff703e1522a8fa723f6
msynn <94026200+msynn@users.noreply.github.com>
2022-01-08 22:14:16 +0800

    list

---
+++ day28.py
@@ -0,0 +1,15 @@
+#Mengeluarkan list beberapa kali
+ucap = ["Hallo Piyan,"
+        "Selamat Malam"]
+
+repeat = 5
+
+lakukan = ucap * repeat
+print(lakukan)
+
+#Menggabungkan list
+a = [1,2,3]
+b = ['apel', 4.0]
+c = [True,'anggur',False]
+gabung = a + b + c
+print(gabung)
