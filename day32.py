8cae7b3685fc9bc497249506120c7697897668e6
msynn <94026200+msynn@users.noreply.github.com>
2022-01-12 20:14:32 +0800

    def

---
+++ day32.py
@@ -0,0 +1,8 @@
+alas = int(input('Masukkan luas alas :'))
+tinggi = int(input('Masukkan tinggi : '))
+
+def luasSegitiga ():
+    luas = 1/2 * alas * tinggi
+    return luas
+
+print('Luas Segitiga :', luasSegitiga())
