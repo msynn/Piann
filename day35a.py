a3ea43f73525f061f769a99db5b259c536c2f495
msynn <94026200+msynn@users.noreply.github.com>
2022-01-15 21:12:43 +0800

    Add files via upload

---
+++ day35a.py
@@ -0,0 +1,10 @@
+def datadiri():
+    nama = input('Masukkan nama : ')
+    umur = int(input('Masukkan Kelas : '))
+    nomorHp = int(input('Masukkan nomor Hp : '))
+
+    print({'Nama' :nama,
+            'Umur': umur,
+            'nomor HP': nomorHp})
+
+datadiri()
