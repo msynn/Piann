e8c8485d88571f0f7da952e7ba25fa72897c0afa
msynn <94026200+msynn@users.noreply.github.com>
2022-01-16 20:30:24 +0800

    Add files via upload

---
+++ day36.py
@@ -0,0 +1,12 @@
+def profil():
+    print('='*5,'My Profil','='*5)
+    name = 'Pian'
+    age = '19 Tahun'
+    status = 'Pelajar'
+    address = 'Topoyo'
+    print('''Name \t: {}
+Age\t: {}
+Status\t: {}
+Address\t: {}''' .format(name,age,status,address))
+
+profil()
