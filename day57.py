5aeed04056f465acb25c8d3c5c52820718d883d3
Fyan <94026200+msynn@users.noreply.github.com>
2022-02-06 21:47:35 +0800

    Add files via upload

---
+++ day57.py
@@ -0,0 +1,7 @@
+# program menghitung luas lingkaran
+
+def luasLingkaran():
+    jari_jari = float(input("Masukkan panjang jari-jari : "))
+    luas = 3.14 * jari_jari**2
+    print(f"{luas} cm")
+luasLingkaran()
